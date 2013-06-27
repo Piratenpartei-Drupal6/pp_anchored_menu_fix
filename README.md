c1c4e3ce92745560b6965cc3d678e9aa81599ae9
nowrap <nowrap@gmx.net>
2013-06-27 19:10:10 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,4 @@
+Drupal 6 Module - Anchored Menu Fix
+===================================
+
+Korrigiert den Active Trail von Menus mit Anchor Links.
---
+++ pp_anchored_menu_fix.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function() {
+	pp_anchored_menu_fix();
+
+	$(window).hashchange(pp_anchored_menu_fix);
+});
+
+function pp_anchored_menu_fix() {
+	var url = $.url();
+	//console.log(url.attr("path"));
+	//console.log(url.attr("fragment"));
+
+	$("ul.menu li a").each(function(index, a) {
+		var link = purl(a);
+		//console.log(link.attr("path"));
+		//console.log(link.attr("fragment"));
+
+		if (link.attr("path") === url.attr("path")) {
+			if (link.attr("fragment") !== url.attr("fragment"))
+				$(a).removeClass("active");
+			else
+				$(a).addClass("active");
+		}
+    });
+}
